561ea327aebc456970247fcf589828765e35a6dc
thanhphong8697 <Thanhphong861997@gmail.com>
2022-12-14 21:04:02 +0700

    up

--- module2/src/DSA/Exercise/ArrayList/MyArrayList.java
+++ module2/src/DSA/Exercise/ArrayList/MyArrayList.java
@@ -1,6 +1,8 @@
 package DSA.Exercise.ArrayList;
 
-public class MyArrayList {
+import java.util.Arrays;
+
+public class MyArrayList<E> {
     /**
      * Số lượng phần tử có trong MyArrayLisst.
      */
@@ -12,19 +14,121 @@ public class MyArrayList {
     /**
      * Mảng chứa các phần tử.
      */
-    Object element[];
+    Object elements[];
     /**
      * Sức chứa mặc định khi khởi tạo contructor không chứa tham số.
      */
     public MyArrayList() {
-        element = new Object[DEFAULT_CAPACITY];
+        elements = new Object[DEFAULT_CAPACITY];
     }
 
+    /**
+     * Phương thức Contructor vói sức chứa được chuyền vào.
+     * @param capacity
+     */
     public MyArrayList(int capacity) {
         if (capacity >= 0){
-            element = new  Object[capacity];
+            elements = new  Object[capacity];
         }else {
-            throw IllegalArgumentException("capacity: " + capacity);
+            throw new IllegalArgumentException("capacity: " + capacity);
         }
+    }
+
+    /**
+     * Phương thức trả về số lượng phần tử.
+     * @return
+     */
+    public int size(){
+        return this.size;
+    }
+
+    /**
+     * Phương thức clear ArrayList.
+     */
+    public void clear(){
+        size = 0;
+        for (int i = 0; i < elements.length; i++){
+            elements[i] = null;
+        }
+    }
+
+    /**
+     * Phương thức add 1 phần tử vào mảng MyArrayList.
+     * @param element
+     * @return
+     */
+    public boolean add(E element){
+        if (elements.length == size){
+            this.ensureCapacity(5);
+        }
+        elements[size] = element;
+        size++;
+        return true;
+    }
+
+    /**
+     * Phương thức tăng kích thước MyArrayList.
+     * @param minCapacity
+     */
+    public void ensureCapacity(int minCapacity){
+        if (minCapacity > 0){
+            int newSize = this.elements.length + minCapacity;
+            elements = Arrays.copyOf(elements, newSize);
+        }else {
+            throw new IllegalArgumentException("minCapacity: " + minCapacity);
+        }
+    }
+    public void add(E element, int index){
+        if (index > elements.length){
+            throw new IllegalArgumentException("index: " + index);
+        }
+        if (elements.length == size){
+            this.ensureCapacity(5);
+        }
+        if (elements[index] == null){
+            elements[index] = element;
+        }else {
+            for (int i = size+1; i >= index; i--){
+                elements[i] = elements[i - 1];
+            }
+            elements[index] = element;
+            size++;
+        }
+    }
+    /**
+     * Phương thức lấy 1 phần tử tại vị trí index.
+     */
+    public E get(int index){
+        return (E) elements[index];
+    }
+
+    /**
+     * Phương thức lấy index của 1 phần tử.
+     * @param element
+     * @return
+     */
+    public int indexOf(E element){
+        int index = -1;
+        for (int i = 0; i < size; i++){
+            if (this.elements[i].equals(element)){
+                return i;
+            }
+        }
+        return index;
+    }
+
+    /**
+     * Phương thức kiểm tra 1 phần tử có trong MyArrayList hay không.
+     * @param element
+     * @return
+     */
+    public boolean contains(E element){
+        return this.indexOf(element) >= 0;
+    }
+    public MyArrayList<E> clone(){
+        MyArrayList<E> v = new MyArrayList<>();
+        v.elements = Arrays.copyOf(this.elements,this.size);
+        v.size = this.size;
+        return v;
     }
 }
---
+++ module2/src/DSA/Exercise/ArrayList/TestMyArrayList.java
@@ -0,0 +1,63 @@
+package DSA.Exercise.ArrayList;
+
+public class TestMyArrayList {
+    public static class Student{
+        private int id;
+        private String name;
+
+        public Student() {
+        }
+
+        public Student(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public static void main(String[] args) {
+            Student student1 = new Student(1,"Nghĩa");
+            Student student2 = new Student(2,"Trường");
+            Student student3 = new Student(3,"Quỳnh");
+            Student student4 = new Student(4,"Quốc");
+            Student student5 = new Student(5,"Đạt");
+            Student student6 = new Student(6,"Khải");
+
+            MyArrayList<Student> studentMyArrayList = new MyArrayList<>();
+            MyArrayList<Student> newStudentMyArrayList = new MyArrayList<>();
+
+            studentMyArrayList.add(student1);
+            studentMyArrayList.add(student2);
+            studentMyArrayList.add(student3);
+            studentMyArrayList.add(student4);
+            studentMyArrayList.add(student5);
+            studentMyArrayList.add(student6,2);
+//            studentMyArrayList.clear();
+            System.out.println(studentMyArrayList.size());
+            for (int i = 0; i < studentMyArrayList.size(); i++){
+                Student student = (Student) studentMyArrayList.elements[i];
+                System.out.println(student.getId());
+                System.out.println(student.getName());
+            }
+            System.out.println(studentMyArrayList.size());
+            System.out.println(studentMyArrayList.get(2).getName());
+//            System.out.println(studentMyArrayList.indexOf(student3));
+            System.out.println(studentMyArrayList.contains(student2));
+            newStudentMyArrayList.clone(studentMyArrayList);
+        }
+    }
+}
